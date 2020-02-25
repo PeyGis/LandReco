33678fe3bfb21b275fd227c6574acab1f6be0d57
PeyGis <isaac.p.coffie@gmail.com>
2020-02-25 10:15:04 +0200

    Asset added

---
+++ Asset.sol
@@ -0,0 +1,69 @@
+pragma solidity 0.5.1;
+
+contract Asset {
+    
+    //Structure for a land
+    struct Land {
+        string size;
+        address owner;
+        uint token_id;
+        uint price;
+        string gps;
+        string details;
+        uint date_created; 
+    }
+    
+    //structure for a building
+    struct Building {
+        string size;
+        address owner;
+        uint token_id;
+        uint price;
+        string gps;
+        string details;
+        uint num_rooms;
+        uint date_created; 
+    }
+     
+    // A map of lands
+    mapping(uint => Land) private landMaps;
+    
+    // A map of Buildings
+    mapping(uint => Building) private buildingMaps;
+    
+    uint private numLands;
+    uint private numBuildings;
+
+    
+    
+    //constructor
+    constructor() public{
+        numLands = 0;
+        numBuildings = 0;
+    }
+    
+    //modifier
+    modifier onlyOwner(address my_address){
+        require(my_address == msg.sender);
+        _;
+    }
+    
+    
+    function createNewLand(string memory _username, address user_addres,  string memory size, uint price, string memory gps, string memory details) public {
+        
+        uint id = keccak256(uint256(now));
+        Land memory thisLand = Land(size, user_addres, id, price, gps, details, now);
+        landMaps[id] = thisLand;
+        numLands+=1;
+    }
+    
+    
+    function createNewBuilding(string memory _username, address user_addres, uint num_rooms, string memory size, uint price, string memory gps, string memory details) public {
+        
+        uint id = keccak256(uint256(now));
+        Building memory thisBuilding = Building(size, user_addres, id, price, gps, details,num_rooms, now);
+        buildingMaps[id] = thisBuilding;
+        numBuildings+=1;
+    }
+    
+}
